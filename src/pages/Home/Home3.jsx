019b46c094aed986d8eb750ce3c8711892ee9121
Alexander Bascuñan <abascunan@jumpitt.com>
2023-07-17 11:35:24 -0400

    publishing proyect

--- src/pages/Home/Home3.jsx
+++ src/pages/Home/Home3.jsx
@@ -187,13 +187,13 @@ function CarouselFadeExample() {
           <h1 className='title-inicio-galeria mt-3'>Galería de fotos</h1>
         </div>
         <div className='img-container'>
-          <div className='container-img-individual'><img onClick={() => openModalImg('Maky 2', '10 Tempura C – 5 Ciboulette – 5 Sésamo – 5 Nori', '_Maky_2-min.JPG')} className='img-menu-home' src={require('./../../assets/img/home/_Maky_2-min.JPG')} alt="ASD" /></div>
+          <div className='container-img-individual'><img onClick={() => openModalImg('Maky 2', '10 Tempura C – 5 Ciboulette – 5 Sésamo – 5 Nori', '_Maky_2-min.JPG')} className='img-menu-home' src={require('./../../assets/img/home/_Maky_2-min.png')} alt="ASD" /></div>
           <div className='container-img-individual'><img onClick={() => openModalImg('Maky 3', '10 Tempura A – 10 Nori – 5 Sésamo – 5 Nori', '_Maky_3-min.JPG')} className='img-menu-home' src={require('./../../assets/img/home/_Maky_3-min.JPG')} /></div>
           <div className='container-img-individual'><img onClick={() => openModalImg('Maky 4', '10 Tempura A – 10 Tempura B – 10 Nori  – 5 Sésamo – 5 Ciboulette', '_Maky_4-min.JPG')} className='img-menu-home' src={require('./../../assets/img/home/_Maky_4-min.jpg')} alt="" /></div>
           <div className='container-img-individual'><img onClick={() => openModalImg('Maky 5', '10 Tempura A – 10 Tempura B – 10 Queso Crema - 10 Nori – 5 Sésamo – 5 Ciboulette', '_Maky_5_v2-min.JPG')} className='img-menu-home' src={require('./../../assets/img/home/_Maky_5_v2-min.JPG')} alt="" /></div>
         </div>
         <div className='img-container'>
-          <div className='container-img-individual'><img onClick={() => openModalImg('Maky 6', '10 Tempura C – 10 Tempura B – 10 Palta - 10 Nori – 5 Sésamo – 5 Ciboulette', '_Maky_6-min.JPG')} className='img-menu-home' src={require('./../assets/img/home/_Maky_6-min.JPG')} alt="" /></div>
+          <div className='container-img-individual'><img onClick={() => openModalImg('Maky 6', '10 Tempura C – 10 Tempura B – 10 Palta - 10 Nori – 5 Sésamo – 5 Ciboulette', '_Maky_6-min.JPG')} className='img-menu-home' src={require('./../../assets/img/home/_Maky_6-min.JPG')} alt="" /></div>
           <div className='container-img-individual'><img onClick={() => openModalImg('Maky 7', '10 Tempura A – 10 Tempura C – 20 Nori – 10 Sésamo – 10 Ciboulette', '_Maky_7-min.JPG')} className='img-menu-home' src={require('./../../assets/img/home/_Maky_7-min.JPG')} alt="" /></div>
           <div className='container-img-individual'><img onClick={() => openModalImg('Tempura F', 'Champiñones - Pollo - Queso Crema', '_Tempura_Champinion_v2-min.JPG')} className='img-menu-home' src={require('./../../assets/img/home/_Tempura_Champinion_v2-min.JPG')} alt="" /></div>
           <div className='container-img-individual'><img onClick={() => openModalImg('Tempura C', 'Pollo - Queso Crema - Cebollín', '_Tempura_C-min.JPG')} className='img-menu-home' src={require('./../../assets/img/home/_Tempura_C-min.JPG')} alt="" /></div>
